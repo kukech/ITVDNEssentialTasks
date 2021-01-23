1e3b025d4b4e1f5970c90b098c583af67425ece8
kukech <qwadik@gmail.com>
2021-01-23 21:41:24 +0300

    доделал таск2, сделал 3 не приводя примера работы словаря, потому что задание аналогично предыдущему, так же сделал 4, оно включено в 2

--- Lesson010/task2/Program.cs
+++ Lesson010/task2/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task2
 {
-//    Создайте класс MyList<T>.Реализуйте в простейшем приближении возможность использования его
-//экземпляра аналогично экземпляру класса List<T>.Минимально требуемый интерфейс
-//взаимодействия с  экземпляром, должен включать метод добавления элемента, индексатор для
-//получения значения элемента по указанному индексу  и свойство только для чтения для получения
-//общего количества элементов.
+    //    Создайте класс MyList<T>.Реализуйте в простейшем приближении возможность использования его
+    //экземпляра аналогично экземпляру класса List<T>.Минимально требуемый интерфейс
+    //взаимодействия с  экземпляром, должен включать метод добавления элемента, индексатор для
+    //получения значения элемента по указанному индексу  и свойство только для чтения для получения
+    //общего количества элементов.
     interface IMyList<T>
     {
         void Add(T varbl);
@@ -31,6 +27,10 @@ namespace task2
                 return list.Length;
             }
         }
+        public MyList()
+        {
+            list = new T[0];
+        }
         public MyList(params T[] arr)
         {
             list = arr;
@@ -44,14 +44,32 @@ namespace task2
             list = templist;
         }
     }
+    static class ExtentionClass
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] temp = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+                temp[i] = (T)list[i];
+            return temp;
+        }
+    }
     class Program
     {
+//        Создайте расширяющий метод: public static T[] GetArray<T>(this MyList<T> list)
+//Примените расширяющий метод к экземпляру типа MyList<T>, разработанному в домашнем задании 2 
+//для данного урока.Выведите на экран значения элементов массива, который вернул расширяющий
+//метод GetArray()
         static void Main(string[] args)
         {
             MyList<int> myList = new MyList<int>(1, 5, 10);
             myList.Add(20);
             for(int i = 0; i < myList.Count; i++)
                 Console.WriteLine(myList[i]);
+
+            int[] array2 = myList.GetArray<int>();
+            for (int i = 0; i < array2.Length; i++)
+                Console.WriteLine(array2[i]);
             Console.ReadKey();
         }
     }
