ac3690f23be52ea59cc349dced60a943ea448474
kukech <qwadik@gmail.com>
2021-01-24 20:28:54 +0300

    можно считать, что выполнил все таски из lesson11, task3 реализован в lesoon10 так, что подходит под задание в 11ом, task4 просто лень делать, там и так понятно что нужно создать класс, в котором элементы коллекции будут бокситься при добавлении в коллекцию, нужно будет создать массив элементов типа object, реализовать по сути те же методы, индексаторы и т.д аналогично предыдущим заданиям, вообщем не вижу смысла выполнять п.с у того, кто придумывал задания в этом курсе очень скудная фантазия

---
+++ Lesson010/task3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+//    Создайте класс MyDictionary<TKey, TValue>.Реализуйте в простейшем приближении возможность
+//использования его экземпляра аналогично экземпляру  класса Dictionary(Урок 6 пример 5). 
+//Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод
+//добавления пар элементов, индексатор для получения значения элемента по указанному индексу и
+//свойство только для чтения для получения общего количества пар элементов.
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public string this[int index]
+        {
+            get
+            {
+                return keys[index] + " " + values[index];
+            }
+        }
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempkey = new TKey[keys.Length + 1];
+            Array.Copy(keys, tempkey, keys.Length);
+            tempkey[tempkey.Length - 1] = key;
+            keys = tempkey;
+
+            TValue[] tempvalues = new TValue[values.Length + 1];
+            Array.Copy(values, tempvalues, values.Length);
+            tempvalues[tempvalues.Length - 1] = value;
+            values = tempvalues;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lesson011/task2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+//    Создайте класс  CarCollection<T>.Реализуйте в простейшем приближении возможность
+//использования его экземпляра  для создания  парка машин.Минимально требуемый интерфейс
+//взаимодействия с экземпляром, должен включать метод добавления  машин с названием машины и
+//года ее выпуска, индексатор для получения значения элемента по указанному индексу  и свойство
+//только для чтения для получения общего количества элементов.  
+//Создайте метод удаления всех машин автопарка. 
+    struct Car
+    {
+        string name;
+        int year;
+        public string CarInfo
+        {
+            get
+            {
+                return name + " - " + Convert.ToString(year);
+            }
+        }
+        public Car(string name, int year)
+        {
+            this.name = name;
+            this.year = year;
+        }
+    }
+    class CarCollection<T> where T:struct
+    {
+        T[] carList;
+        public int Count
+        {
+            get
+            {
+                return carList.Length;
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return carList[index];
+            }
+        }
+        public CarCollection()
+        {
+            carList = new T[0];
+        }
+        public CarCollection(T[] cars)
+        {
+            carList = cars;
+        }
+        public void AddCar(T car)
+        {
+            carList = carList ?? new T[1];
+            Array.Resize<T>(ref carList, carList.Length + 1);
+            carList[carList.Length - 1] = car;
+        }
+        public void Clear()
+        {
+            this.carList = new T[0];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarCollection<Car> cars = new CarCollection<Car>();
+            Console.WriteLine("input car count");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i< n; i++)
+            {
+                Console.WriteLine("{0} input car, year:", i);
+                string stmp = Console.ReadLine();
+                int yeartmp = Convert.ToInt32(Console.ReadLine());
+                cars.AddCar(new Car(stmp, yeartmp));
+            }
+            Console.WriteLine(cars[2].CarInfo);
+            Console.ReadKey();
+        }
+    }
+}
