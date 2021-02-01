970e1a86a60cd8333927e9ca4083a62ac4ddd6c0
kukech <qwadik@gmail.com>
2021-02-01 23:30:26 +0300

    упс, забыл добавить, то что сделал. пысы надо посмотреть как удалять лишние снимки репозитория

---
+++ Lesson014/task2/MyList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        T[] array;
+        int position = -1;
+        public MyList()
+        {
+            this.array = new T[0]; 
+        }
+        public MyList(T[] arr)
+        {
+            array = arr;
+        }
+        public int Count { 
+            get
+            {
+                return array.Length;
+            } 
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+        public void Add(T toadd)
+        {
+            //в прошлых заданиях делал, лень делать это говно
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+        public bool MoveNext()
+        {
+            if (position < array.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Reset()
+        {
+            this.position = -1;
+        }
+        public T Current 
+        {
+            get
+            {
+                return array[position];
+            }
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                return array[position];
+            }
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Lesson014/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+//    Создайте коллекцию MyList<T>.Реализуйте в простейшем приближении возможность использования
+//ее экземпляра аналогично экземпляру класса List<T>.Минимально требуемый интерфейс
+//взаимодействия с экземпляром, должен включать метод добавления элемента, индексатор для
+//получения значения элемента по указанному индексу и свойство только для чтения для получения
+//общего количества элементов. Реализуйте возможность перебора элементов коллекции в цикле
+//foreach
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = new string[3] { "safdsdf", "sdfsdfsdf", "sdfsdfsdf" };
+            MyList<string> list = new MyList<string>(s);
+            foreach(string i in list)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
